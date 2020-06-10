31f33502e54bcd9e2f74caa9238ae54c12406835
alkee-allm <alkee@allm.co.kr>
2020-06-10 18:31:21 +0900

    모든 서버에 동일안 SecurityKey 를 갖도록 하는 로직 구성

--- K2svc/Frontend/InitService.cs
+++ K2svc/Frontend/InitService.cs
@@ -11,8 +11,10 @@ namespace K2svc.Frontend
 {
     public class InitService : Init.InitBase
     {
-        // SecurityKey의 경우 모든 서버가 같은 key 를 갖도록 설정해야 할 것.
-        public static SymmetricSecurityKey SecurityKey { get; } = new SymmetricSecurityKey(Guid.NewGuid().ToByteArray());
+        private static string VERSION = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(2); // major.minor
+        // ** 주의 **  ; https://github.com/alkee-allm/k2proto/issues/2#issuecomment-641868442
+        //   모든 실행되는 서버들은 Major+minor 버전이 일치해야한다.
+        public static SymmetricSecurityKey SecurityKey = new SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes("hash salt +" + VERSION));
 
         private readonly ILogger<InitService> logger;
 
