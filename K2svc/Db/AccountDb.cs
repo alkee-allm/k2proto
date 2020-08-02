aec2ace77a6de87405c6ee72e7f3b48cc30bcd0a
alkee-allm <alkee@allm.co.kr>
2020-08-02 16:42:38 +0900

    AccountDb 를 추가하고 이를 이용하 id-pw 인증

---
+++ K2svc/Db/AccountDb.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Reflection;
+
+namespace K2svc.Db
+{
+    public class AccountDb
+        : DbContext // https://docs.microsoft.com/ko-kr/ef/core/miscellaneous/configuring-dbcontext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public AccountDb(ServiceConfiguration _config)
+        {
+            config = _config;
+            Database.EnsureCreated();
+
+#if DEBUG
+            // TEST environment
+            if (Accounts.Count() == 0)
+            {
+                Accounts.AddRange(new Account[] {
+                    new Account { AccountName = "k1", Password = "k1k1" },
+                    new Account { AccountName = "k2", Password = "k2k2" },
+                    new Account { AccountName = "k3", Password = "k3k3" },
+                    new Account { AccountName = "k4", Password = "k4k4" },
+                });
+                SaveChanges();
+            }
+#endif
+        }
+
+        #region DbContext override
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            // database provider 선택 ; 하드코딩없이 간편하게 
+            builder.UseSqlite($"filename={config.DatabaseFileName}", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // TODO: 하드코딩하지 않고 table 을 늘리거나 변경할 수 있도록(Attribute 이용?)
+            Account.OnModelCreating(builder.Entity<Account>());
+
+            base.OnModelCreating(builder);
+        }
+        #endregion
+
+        private readonly ServiceConfiguration config;
+    }
+
+}
---
+++ K2svc/Db/Models.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders; // for EntityTypeBuilder
+
+namespace K2svc.Db
+{
+    public class Account
+    {
+        public long Id { get; set; }
+        public string AccountName { get; set; }
+        public string Password { get; set; }
+
+        internal static void OnModelCreating(EntityTypeBuilder<Account> builder)
+        {
+            builder.HasKey(e => e.Id); // primary key
+            builder.Property(e => e.Id).ValueGeneratedOnAdd(); // auto increment
+            builder.HasIndex(e => e.AccountName).IsUnique(); // unique index
+        }
+    }
+}
