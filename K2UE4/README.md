0cc1e50e6f12f5d32b202ba92f12f815a8a345c2
ReU4752 <liyu4752@gmail.com>
2020-09-08 12:32:07 +0900

    protobuf, grpc 라이브러리 추가

--- K2UE4/README.md
+++ K2UE4/README.md
@@ -4,3 +4,9 @@ Unreal Engine 4 에서 [gRPC](https://grpc.io)를 이용해 [서버](../K2svc/RE
 
 1. 엔진 버전  
 Unreal Engine 4 **4.25.3**
+
+### Build and run
+ 1. UE4 protobuf, grpc 빌드를 위한 라이브러리 추가
+
+    1) 프로젝트 루트에 ThirdParty 폴더 생성  
+    2) vcpkg/packages 안에 앞서 설치한 protobuf / grpc 폴더 복사하여 ThirdParty 안에 붙여넣기
--- K2UE4/Source/K2UE4/K2UE4.Build.cs
+++ K2UE4/Source/K2UE4/K2UE4.Build.cs
@@ -1,9 +1,21 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System;
+using System.IO;
 using UnrealBuildTool;
 
 public class K2UE4 : ModuleRules
 {
+	private string ModulePath
+	{
+		get { return ModuleDirectory; } 
+	}
+	
+	private string ThirdPartyPath
+	{ 
+		get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); } 
+	}
+	
 	public K2UE4(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
@@ -12,6 +24,10 @@ public class K2UE4 : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+		LoadThirdPartyLibrary("protobuf", Target);
+		LoadThirdPartyLibrary("grpc", Target);
+		PublicDefinitions.Add(string.Format("WITH_GRPC_BINDING=1"));
+		
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
@@ -19,5 +35,26 @@ public class K2UE4 : ModuleRules
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+	
+	public void LoadThirdPartyLibrary(string libraryName, ReadOnlyTargetRules Target, bool dynamic = false) {
+		bool isLibrarySupported = false;
+		if (Target.Platform == UnrealTargetPlatform.Win64) {
+			isLibrarySupported = true;
+			string PlatformString = "x64";
+			string libraryDir = libraryName + "_" + PlatformString + "-windows";
+			
+			if (!dynamic) {
+				string LibrariesPath = Path.Combine(ThirdPartyPath, libraryDir, "lib");
+				DirectoryInfo d = new DirectoryInfo(LibrariesPath);
+				FileInfo[] Files = d.GetFiles("*.lib");
+                foreach (FileInfo file in Files)
+                {
+                    PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, file.Name));
+				}
+			}
+
+			PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, libraryDir, "include"));
+		}
 	}
 }
