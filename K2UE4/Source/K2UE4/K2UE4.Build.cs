a2f3da107fea57827b8ae4d9368eae440212654f
ReU4752 <liyu4752@gmail.com>
2020-09-11 11:07:43 +0900

    Generate한 proto 파일들 저장 경로 IncludePath 에 추가

--- K2UE4/Source/K2UE4/K2UE4.Build.cs
+++ K2UE4/Source/K2UE4/K2UE4.Build.cs
@@ -14,12 +14,14 @@ public class K2UE4 : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+		PublicIncludePaths.Add("K2UE4/Proto");
+
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-    }
+	}
 }
