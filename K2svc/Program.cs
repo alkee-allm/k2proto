435b57a1f8b9fb6f706834defc9506de21088a23
alkee-allm <alkee@allm.co.kr>
2020-09-14 12:58:14 +0900

    single-file publish 시에도 appsettings 를 잘 읽을 수 있도록

--- K2svc/Program.cs
+++ K2svc/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace K2svc
@@ -14,6 +15,13 @@ namespace K2svc
             public static readonly int INVALID_ARGUMENT = 1;
         }
 
+        private const string APP_SETTINGS_FILENAME = "appsettings.json";
+
+        private static readonly ILogger<Program> logger = LoggerFactory.Create(builder =>
+        { // https://docs.microsoft.com/ko-kr/aspnet/core/fundamentals/logging/?view=aspnetcore-3.1#logging-providers
+            builder.AddConsole();
+        }).CreateLogger<Program>();
+
         public static int Main(string[] args)
         {
             // https://docs.microsoft.com/en-us/aspnet/core/grpc/troubleshoot?view=aspnetcore-3.0#call-insecure-grpc-services-with-net-core-client
@@ -25,13 +33,38 @@ namespace K2svc
             return AppExitCode.OK;
         }
 
+        private static string CurrentProcessPath
+            => System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule?.FileName);
+
+        private static string GetBasePath()
+        { // https://github.com/alkee-allm/k2proto/issues/55
+            // 설정 파일은 작업경로 및 실행파일 경로 어느쪽에 있든 동작 가능하도록
+            var cd = System.IO.Directory.GetCurrentDirectory();
+            var pd = CurrentProcessPath;
+            if (cd == pd) return cd;
+
+            logger.LogInformation($"WORKING DIRECTORY : {cd}");
+            logger.LogInformation($"PROCESS DIRECTORY : {pd}");
+
+            bool configInCurrentDir = System.IO.File.Exists(System.IO.Path.Combine(cd, APP_SETTINGS_FILENAME));
+            bool configInProcessDir = System.IO.File.Exists(System.IO.Path.Combine(pd, APP_SETTINGS_FILENAME));
+
+            if (configInCurrentDir && configInProcessDir)
+            { // 어느쪽 파일을 사용하는지 직관적이지 않으므로, logging 에라도 표현
+                logger.LogWarning($"{APP_SETTINGS_FILENAME} EXISTS in BOTH of working and process directoroes. Using the file in WORKING DIRECTORY.");
+                return cd;
+            }
+
+            return configInCurrentDir ? cd : pd;
+        }
+
         private static K2Config LoadConfig(string[] args)
         {
-            const string APP_SETTINGS_FILENAME = "appsettings.json";
             string APP_SETTINGS_OVERRIDE_FILENAME = $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json";
 
             // build configuration ; https://stackoverflow.com/a/58594026
-            var builder = new ConfigurationBuilder()
+            var builder = new ConfigurationBuilder() // configuration 순서 중요
+                .SetBasePath(GetBasePath()) // 파일을 읽기 전에 설정
                 .AddEnvironmentVariables() // 환경변수에 "K2:ServerId" 와 같은 형식의 값으로 사용 가능하도록
                 .AddJsonFile(APP_SETTINGS_FILENAME); // 환경변수 값을 json 으로 override 가능하도록
             if (System.IO.File.Exists(APP_SETTINGS_OVERRIDE_FILENAME)) builder.AddJsonFile(APP_SETTINGS_OVERRIDE_FILENAME); // 환경에 따른 json override
